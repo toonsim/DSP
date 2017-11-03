d057976290680e0c045e668a23c076a5507eee1b
Tris <Tris@192.168.0.171>
2017-11-03 21:50:17 +0100

    Woop Woop

--- ber.m
+++ ber.m
@@ -1,6 +1,11 @@
 function [ errorprob ] = ber( input, output )
     %BER Summary of this function goes here
     %   Detailed explanation goes here
-    errorprob = sum(abs(input-output))/length(input);
+    
+    % Correct but there is a matlab function for this
+    %errorprob = sum(abs(input-output))/length(input);
+    
+    No_errors = biterr(input,output);
+    errorprob = No_errors/length(input);
 end
 
